015ae1ac40d6a317494de9dfc2e69e2b98e3fd8f
leonyux <leon.yux@gmail.com>
2014-07-04 16:49:47 +0800

    cscope shortcut modified

--- scripts/cscope.sh
+++ scripts/cscope.sh
@@ -4,7 +4,7 @@ MPWD=`pwd`
 if [ $1 ]; then
     rm -rf cscope
     mkdir cscope
-    find ${MPWD}/FDDBBU -regex ".*\.c*[chx]" > cscope/cscope.files
+    find ${MPWD}/$1 -regex ".*\.c*[chx]" > cscope/cscope.files
     cd cscope
     cscope -bq
     cd ${MPWD}
@@ -13,12 +13,3 @@ fi
 CSCOPE_DB=${MPWD}/cscope/cscope.out
 export CSCOPE_DB
 
-
-#alias for FDDBBU
-BRANCH_HOME=$MPWD/FDDBBU/1Develope_Case/HX-BPU_R3
-export BRANCH_HOME
-#branch alias
-alias osp="cd $BRANCH_HOME/firm/firmware/platform/osp/libs"
-alias firm="cd $BRANCH_HOME/firm"
-alias oam="cd $BRANCH_HOME/flash/software/platform/oam"
-alias l3="cd $BRANCH_HOME/flash/software/l3/test/linux_proj"
